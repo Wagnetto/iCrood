1c5e9006f40f54006d8b874bf82fd8d7f81a484e
Hélio Fabricio <65051794+HelioFabricio@users.noreply.github.com>
2023-06-15 21:02:53 -0300

    testando-classe-cadastro

---
+++ iCrood/src/Cadastro.java
@@ -0,0 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Cadastro(){
+    private List<Client> clientes = new ArrayList<>;
+
+    public void inserirDados(){
+    Scanner scan = new Scanner(System.in);
+    String nome = "", cpf = "", dataNascimento = "", endereco = "", numeroTelefone = "", email = "";
+
+    System.out.println("Entre seu Nome: ");
+    nome = scan.nextLine();
+    System.out.println("Entre seu CPF: ");
+    cpf = scan.nextLine();
+    System.out.println("Entre sua Data de Nascimento: ");
+    dataNascimento = scan.nextLine();
+    System.out.println("Entre seu endereço: ");
+    endereco = scan.nextLine;
+    System.out.println("Entre seu Numero de Telefone: ");
+    numeroTelefone = scan.nextLine;
+    System.out.println("Entre seu E-mail: ");
+    email = scan.nextLine;
+    }
+    public void cadastrar(){
+        Client
+    }
+}
