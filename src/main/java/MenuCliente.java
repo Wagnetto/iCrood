71181b60dd584b3a622499d654bd083088bf4a2f
Wagner Silveira Souto <thatsme@Inspiron-5548-c86f5b39.localdomain>
2023-06-23 10:11:36 -0300

    Cria classe pedido e altera MenuCliente

---
+++ src/main/java/MenuCliente.java
@@ -0,0 +1,54 @@
+import java.time.LocalDateTime;
+import java.util.Scanner;
+
+class MenuCliente {
+    static Scanner input = new Scanner(System.in);
+    private Client cliente;
+    private Restaurante restaurante;
+    private boolean entregue;
+    private Vendor vendor;
+
+    public void realizarEntrega(String cidade) {
+        if (cidade.equalsIgnoreCase("Porto Alegre") || cidade.equalsIgnoreCase("POA")) {
+            // Lógica para realizar a entrega
+            this.entregue = true;
+            System.out.println("Pedido realizado com sucesso!");
+
+            // Salvar a entrega no arquivo de dados
+            SalvarDados entrega = new SalvarDados(this.cliente.getIdCliente(), this.restaurante.getIdRestaurante(), LocalDateTime.now());
+            entrega.salvarEntrega();
+
+        } else {
+            System.out.println("Desculpe, as entregas só podem ser feitas em Porto Alegre.");
+        }
+    }
+
+    public void exibirListaRestaurantes(int i) { // exibe todos os restaurantes puxando de Cadastro
+        for (LerDados.Restaurante restaurante : LerDados.lerRestaurantesDoArquivo()) {
+            System.out.println(i + ". " + restaurante.getNome());
+            i++;
+        }
+    }
+
+    public void exibirMenuClienteJacadastrado() {
+        System.out.println("Digite 1 para procurar um restaurante e fazer pedido.\nDigite 2 para visualizar seu histórico de pedidos");
+        int opcaoMenu = input.nextInt();
+        if(opcaoMenu == 1){
+            exibirListaRestaurantes(1);
+            Pedido.selecionaRestaurante();
+        } else if(opcaoMenu ==2) {
+            exibirHistoricoCliente();
+        }
+
+    }
+
+    //TODO listar os pedidos de um cliente VICTOR integrar BANCO
+    public void exibirHistoricoCliente(){
+
+    }
+
+
+
+
+
+}
---
+++ src/main/java/Pedido.java
@@ -0,0 +1,88 @@
+import java.util.List;
+import java.util.Scanner;
+
+public class Pedido {
+
+    //Declarando aqui LISTA simples com os possíveis estados pra evitar divergência
+    static final String [] statusPossiveis = {"0 - Em produção", "1 - Pronto", "2 - Em trânsito", "3 - Entregue"};
+
+    int idPedido;
+
+
+    //TODO essa lista deve ser uma lista de objetos produto?
+    // ou uma lista dos IDs deles, que apenas apontam para um produto registrado no Banco ?
+    private List<Produto> carrinho;
+
+
+    //TODO VICTOR: como fazer para vincular o id de um restaurante existente a um pedido. SEM o usuário ter qu digitar no construtor?
+    int idRestaurante;
+
+    double valorTotalPedido;
+
+    public void setValorTotalPedido(double valorTotalPedido) {
+        this.valorTotalPedido = valorTotalPedido;
+    }
+
+    public void setStatusPedido(String statusPedido) {
+        this.statusPedido = statusPedido;
+    }
+
+    String statusPedido;
+
+    //generate constructor
+
+    public static Restaurante selecionaRestaurante() {
+        System.out.println("Seleciona restaurante foi chamado");
+        System.out.println("Digite o Número do Restaurante");
+        Scanner input = new Scanner(System.in);
+        int entrada = input.nextInt();
+
+        List<LerDados.Restaurante> restaurantes = LerDados.lerRestaurantesDoArquivo();
+
+        if (entrada >= 1 && entrada <= restaurantes.size()) {
+            // Restaurante válido selecionado
+            LerDados.Restaurante restauranteSelecionado = restaurantes.get(entrada - 1);
+            System.out.println("Restaurante selecionado: " + restauranteSelecionado.getNome());  //OK até aqui
+
+
+            // TODO VICTOR | HELIO (não seria melhor retornar id do Restaurante ?) Retorna o restaurante selecionado
+            return null;
+        } else {
+            System.out.println("Opção inválida. Tente novamente.");
+
+            // Chamada recursiva para selecionar novamente o restaurante, (fallback)
+            return selecionaRestaurante();
+        }
+
+
+    }
+
+    public double calcularPrecoTotal() {
+        double precoTotal = 0.0;
+
+        for (Produto produto : carrinho) {
+            precoTotal += produto.getValor();
+            setValorTotalPedido(precoTotal);
+
+        }
+
+        return precoTotal;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("---------------------------------\n");
+        sb.append("Seu pedido:\n\n");
+        for (Produto produto : carrinho) {
+            sb.append(produto.toString()).append("\n");
+        }
+        sb.append("\nPreço total: R$").append(calcularPrecoTotal());
+        return sb.toString();
+    }
+
+
+
+
+
+}
